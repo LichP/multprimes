47be1efe9942f1fd56c3fe648c6f7b5a004a8bfb
Phil Stewart <phil.stewart@lichp.co.uk>
2012-11-14 18:06:36 +0000

    Document MultTable

--- lib/mult_table.rb
+++ lib/mult_table.rb
@@ -7,6 +7,12 @@ class MultTable
     @multipliers = multipliers.flatten.sort
   end
   
+  # Compute a line of the multiplication table
+  #
+  # @param [Integer] i the number of the line to compute
+  # @return [Array<Integer>] of products calculated from the +i+th multiplier
+  # @return [Array<Integer>] of untouched multipliers if +i = 0+
+  # @return [nil] if +i+ is out of range or otherwise invalid
   def line(i)
     case i
       when 0
@@ -18,16 +24,23 @@ class MultTable
     end
   end
   
+  # @return #line(0)
   def header_line
     self.line(0)
   end
   
+  # Generate all lines in the multiplication table (except for the header)
+  #
+  # @return [Array<Integer, Array<Integer>] an array of lines, each line
+  # consisting of the multiplier and an array of products of that multiplier
   def lines
     1.upto(multipliers.length).collect do |i|
       [self.multipliers[i - 1], self.line(i)]
     end
   end
   
+  # @return [Integer] the largest product in the table, i.e. the square of
+  # the last (therefore largest) multiplier
   def largest_product
     self.multipliers.last ** 2
   end
