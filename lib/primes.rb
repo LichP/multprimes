ba738cf2591286702aca210332823cacb015dfb2
Phil Stewart <phil.stewart@lichp.co.uk>
2012-11-14 11:32:58 +0000

    Added check to ensure Primes.first returns nil if not passed a positive integer

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,5 +1,5 @@
 module Primes
   def self.first(count)
-    
+    return nil unless count.kind_of?(Integer) && count > 0
   end
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -1,8 +1,14 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'primes')
 
 describe Primes do
-  describe ".first(3)" do
-    it "should return the first three primes" do
+  describe ".first(n)" do
+    it "should return nil if n is not a positive integer" do
+      Primes.first("three").should be_nil
+      Primes.first(-1).should      be_nil
+      Primes.first(0).should       be_nil
+    end
+    
+    it "should return the first n primes" do
       Primes.first(3).should == [2, 3, 5]
     end
   end
