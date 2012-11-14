6c38ac9a74b91935a97e1808e32a3cd204b6d2a3
Phil Stewart <phil.stewart@lichp.co.uk>
2012-11-14 13:28:21 +0000

    Implement Primes.initial_sieve_for(count)

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,5 +1,41 @@
 module Primes
-  def self.first(count)
-    return nil unless count.kind_of?(Integer) && count > 0
+  class << self
+    
+    # Find the first +count+ prime numbers.
+    #
+    # @param [Integer] count the number of primes to find
+    # @return [Array<Integer>] if +count+ is a positive integer
+    # @return [nil] otherwise
+    def first(count)
+      return nil unless count.kind_of?(Integer) && count > 0
+      
+      # To find +count+ primes, we need to sieve a set of integers large enough
+      # to contain +count+ primes, i.e. +n : pi(n) == count+. Use 
+      sieve = (2..count).to_a
+    end
+
+    # Create an initial sieve containing the first +count+ prime numbers.
+    #
+    # @param [Integer] count the number of primes to be contained in the sieve
+    # @return [Array<Integer>] if +count+ is a positive integer
+    # @return [nil] otherwise
+    def initial_sieve_for(count)
+      return nil unless count.kind_of?(Integer) && count > 0
+      
+      # To find +count+ primes, we need to sieve a set of integers large enough
+      # to contain +count+ primes. We use the upper bound nth prime < n * (ln n  +
+      # ln ln n) for n >= 6
+      bound = if count >= 6
+        (
+          (
+            Math.log(count) + Math.log(Math.log(count))
+          ) * count
+        ).ceil
+      else
+        12
+      end
+
+      (2..bound).to_a
+    end
   end
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -12,4 +12,29 @@ describe Primes do
       Primes.first(3).should == [2, 3, 5]
     end
   end
+  
+  describe ".initial_sieve_for(n)" do
+    it "should return nil if n is not a positive integer" do
+      Primes.initial_sieve_for("three").should be_nil
+      Primes.initial_sieve_for(-1).should      be_nil
+      Primes.initial_sieve_for(0).should       be_nil
+    end
+    
+    it "should return [2..12] for n < 6" do
+      sieve = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+      1.upto(5) { |i| Primes.initial_sieve_for(i).should == sieve }
+    end
+    
+    it "should return [2..15] for n == 6" do
+      Primes.initial_sieve_for(6).should == (2..15).to_a
+    end
+    
+    it "should return [2..32] for n == 10" do
+      Primes.initial_sieve_for(10).should == (2..32).to_a
+    end
+    
+    it "should return [2..614] for n == 100" do
+      Primes.initial_sieve_for(100).should == (2..614).to_a
+    end
+  end
 end
